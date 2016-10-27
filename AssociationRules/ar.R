1c7ec0f689a31b29cc8387edb1584008cfd439a3
Hai <haining@microsoft.com>
2016-10-27 00:47:51 -0400

    association rules multi-targets

--- AssociationRules/ar.R
+++ AssociationRules/ar.R
@@ -15,10 +15,12 @@ ar <- function(dataframe,
                target = "rules",
                maxReturnItems = 100) {  
   # initialization
+  print ("initialization")
   trx = NULL
   class(minSupport) <- NULL
   class(minConfidence) <- NULL
   
+  print ("get columns")
   # if no columns specified, take all columns
     if (is.null(cols)) {
       cols = colnames(dataframe)
@@ -26,12 +28,14 @@ ar <- function(dataframe,
   
   # data object is a regular data frame
   if (dataObjType == 'dataframe') {        
+    print ("for dataframe, convert all columns to factor")
     # convert all columns to factor
     df <- as.data.frame(lapply(dataframe[cols], as.factor))    
     trx <- as(df, "transactions")
   } 
   # data object is a single column where each row is a list of items separated by comma
   else if (dataObjType == "list") {        
+    print ("for list, split items in the items column into list")	
     if (length(cols) != 1) stop("There can be only a single column.")    
     df <- dataframe[cols]    
     l <- lapply(apply(df, 1, mySplit), str_trim)    
@@ -39,15 +43,19 @@ ar <- function(dataframe,
   }
   # data object is a sparse matrix
   else if (dataObjType == "matrix") {
-    m <- as.matrix(dataframe)
+    print ("for matrix, just cast it to transactions")
+    m <- as.matrix(dataframe[cols])
     trx <- as(m, "transactions")        
   }
   else {
     stop (paste("Unknown data object type:", dataObjType))
   }
   
+  print ("construct parameters")
   # parameter list. set minlen = 2 to avoid empty rule sets
-  p <- list(supp = minSupport, confidence = minConfidence, minlen = minlen, maxlen = maxlen, target = target)   
+  p <- list(supp = minSupport, confidence = minConfidence, minlen = minlen, maxlen = maxlen, target = as.character(target))
+
+  print ("construct appearance")
   # appearance list
   a <- list()
   if (!is.null(rhs)){ 
@@ -60,8 +68,12 @@ ar <- function(dataframe,
     }     
   if (!is.null(lhs) & !is.null(rhs)) { a$default <- "none" }
   
+  print ("run rules")
   rules <- apriori(trx, parameter = p, appearance = a)  
+
+  print ("calculate top N")
   maxReturnItems <- if (length(rules) < maxReturnItems) length(rules) else maxReturnItems
+  print (maxReturnItems)
   
   str(rules)   
   # find left and right side labels
@@ -71,45 +83,58 @@ ar <- function(dataframe,
     left = character()
     right = character()
     
-    # prune the rules if asked  
+    print ("prune rules")
     if (length(rules) > 0) {
+      # prune the rules if asked  
       if (length(rules) > 1 & pruneRedundancies) {
         sub <- is.subset(rules, rules)
         sub[lower.tri(sub, diag = T)] <- NA
         redundant <- colSums(sub, na.rm = T) >= 1
         rules <- rules[!redundant]
       }
+
       # sort the rules
+      print ("sort rules")
       rules <- sort(rules, by = sortBy)
-      rules <- rules[1:maxReturnItems]
-      
+      print ("get left hand side")
       left = labels(lhs(rules))$elements
+      print ("get right hand side")
       right = labels(rhs(rules))$elements
     }
     # print the rules
+    print ("inspect rules")
     inspect(rules)   
     # create an array as row index
+    print ("create row index arrary")
     ids <- if (length(rules) > 0) 1:length(rules) else integer()
     # assemble the rules dataset
-    rulesDataset <- data.frame(id = ids, lhs = left, rhs = right, rules@quality) 
-    return (rulesDataset)  
+    print ("assemble rules dataset")
+    result <- data.frame(id = ids, lhs = left, rhs = right, rules@quality) 
   } 
   # return frequent item sets
   else { 
     # always sort by support
     if (length(rules) > 1) {
+      print ("always sort by support")
       rules <- sort(rules, by = "support")
-      rules <- rules[1:maxReturnItems]
     }
+    print ("inspect rules")
     inspect(rules) 
     # set up an array for row index
+    print ("create row index arrary")
     ids <- if (length(rules) > 0) 1:length(rules) else integer()
     # set up an array for items
+    print ("get items list")
     labels <- if (length(rules) == 0) character() else labels(items(rules))$elements
     # assemble the dataframe
-    itemsDataset <- data.frame(id = ids, items = labels, rules@quality)
-    return (itemsDataset)
+    print ("assemble items dataset")
+    result <- data.frame(id = ids, items = labels, rules@quality)
   }
+  if (maxReturnItems > 1) {
+    print ("take top N")
+    result <- result[1:maxReturnItems,]
+  }
+  return (result)
 }
 
 mySplit <- function(s) strsplit(s, ",")[[1]]
---
+++ BuildCustomModuleAndUpload.ps1
@@ -0,0 +1,26 @@
+﻿$SourceFolder    = "D:\Temp\git\custmod\AssociationRules"
+$DestinationFile = "D:\Temp\git\custmod\AssociationRules\ar.zip"
+$Compression     = "Optimal"  # Optimal, Fastest, NoCompression
+
+if (Test-Path $DestinationFile)
+{
+    Remove-Item $DestinationFile
+}
+
+function Zip-Directory {
+    Param(
+      [Parameter(Mandatory=$True)][string]$DestinationFileName,
+      [Parameter(Mandatory=$True)][string]$SourceDirectory,
+      [Parameter(Mandatory=$False)][string]$CompressionLevel = "Optimal",
+      [Parameter(Mandatory=$False)][switch]$IncludeParentDir
+    )
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $CompressionLevel    = [System.IO.Compression.CompressionLevel]::$CompressionLevel  
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($SourceDirectory, $DestinationFileName, $CompressionLevel, $IncludeParentDir)
+}
+
+Zip-Directory -DestinationFileName $DestinationFile -SourceDirectory $SourceFolder -CompressionLevel $Compression #-IncludeParentDir #Optional parameter
+
+Import-Module .\AzureMLPS.dll
+New-AmlCustomModule -ConfigFile .\config_sichuan.json -CustomModuleZipFileName $DestinationFile
+#New-AmlCustomModule -ConfigFile .\config_sichuan.json -CustomModuleZipFileName 'C:\Users\haining\Desktop\Temp\custom modules\AddManyColumns.zip' 
